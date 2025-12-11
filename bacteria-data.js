b050ff4733b038f17f751261d7ec28599a0bb877
gabrielsodre44-collab <gabrielsodre44@gmail.com>
2025-12-11 18:49:52 -0300

    bacteria-data.js

---
+++ bacteria-data.js
@@ -0,0 +1,23 @@
+const bacteriaDatabase = [
+    {
+        nome: "Escherichia coli",
+        gram: "Gram-negativa",
+        morfologia: "Bacilo",
+        testes: ["Indol +", "Lactose +", "Citrato -"],
+        importancia: "Infecções intestinais e urinárias"
+    },
+    {
+        nome: "Salmonella enterica",
+        gram: "Gram-negativa",
+        morfologia: "Bacilo",
+        testes: ["Lactose -", "H2S +", "Urease -"],
+        importancia: "Gastroenterite e febre tifoide"
+    },
+    {
+        nome: "Klebsiella pneumoniae",
+        gram: "Gram-negativa",
+        morfologia: "Bacilo encapsulado",
+        testes: ["Urease +", "Lactose +"],
+        importancia: "Infecções respiratórias e urinárias"
+    }
+];
